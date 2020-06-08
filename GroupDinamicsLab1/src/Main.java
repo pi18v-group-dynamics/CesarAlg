1963c6919c5b384070e5bd0bca7d47d1f1b46778
nastekoir <nastekoir@gmail.com>
2020-06-08 19:27:25 +0300

    Добавил расшифровку

--- GroupDinamicsLab1/src/Main.java
+++ GroupDinamicsLab1/src/Main.java
@@ -5,13 +5,27 @@ import java.util.jar.JarFile;
 public class Main extends JFrame {
 
 	public static void main(String[] args){
-
+		System.out.println("1-зашифровка\n2-расшифровка");
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Введите сообщение которое надо зашифровать");
-		String t=scanner.nextLine();
-		System.out.println("Введите ключ");
-		x=scanner.nextInt();
-		System.out.println("Зашифрованное сообщение "+script(t));
+		int enter=scanner.nextInt();
+		if(enter==1){
+			scanner = new Scanner(System.in);
+			System.out.println("Введите сообщение которое надо зашифровать");
+			String t=scanner.nextLine();
+			scanner = new Scanner(System.in);
+			System.out.println("Введите ключ");
+			x=scanner.nextInt();
+			System.out.println("Зашифрованное сообщение "+script(t));
+		}
+		else{
+			scanner = new Scanner(System.in);
+			System.out.println("Введите сообщение которое надо зашифровать");
+			String t=scanner.nextLine();
+			scanner = new Scanner(System.in);
+			System.out.println("Введите ключ");
+			x=scanner.nextInt();
+			System.out.println("Зашифрованное сообщение "+descript(t));
+		}
 	}
 
 	static char[] alfavitEn=new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
@@ -31,6 +45,20 @@ public class Main extends JFrame {
 		}
 		return String.valueOf(str);
 	}
-
+	static public String descript(String y){
+		char[] str=y.toCharArray();
+		int j=0;
+		for(int i=0;i<y.length();i++){
+			if(str[i]==' ') i++;
+			while(alfavitEn[j]!=str[i])
+				j++;
+			int n=j-x;
+			if(n<0)str[i]=alfavitEn[n+26];
+			else if(n<alfavitEn.length)
+				str[i]=alfavitEn[n];
+			j=0;
+		}
+		return String.valueOf(str);
+	}
 }
 
