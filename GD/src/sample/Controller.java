73245e7b6d0927769b4d1bed6fb8075f06eaaa52
Vizhosky <57767076+Vizhosky@users.noreply.github.com>
2020-06-13 18:48:02 +0300

    Добавил авторизацию, форму авторизации, главную форму, а так же привязал методы расшифровки с шифровкой к кнопкам.

---
+++ GD/src/sample/Controller.java
@@ -0,0 +1,63 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    static char[] alfavitEn=new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    public TextArea TextAreaOutput;
+    public TextArea TextAreaInput;
+    public TextField TextFieldKey;
+
+    public void ButtonDecrypt(ActionEvent actionEvent) {
+        char[] str=TextAreaInput.getText().toLowerCase().toCharArray();
+        int j=0;
+        for(int i=0;i<TextAreaInput.getText().length();i++){
+            if(str[i]==' ') i++;
+            while(alfavitEn[j]!=str[i])
+                j++;
+            int n=j-Integer.parseInt(TextFieldKey.getText());
+            if(n<0)str[i]=alfavitEn[n+26];
+            else if(n<alfavitEn.length)
+                str[i]=alfavitEn[n];
+            j=0;
+        }
+        TextAreaOutput.setText(String.valueOf(str));
+    }
+
+    public void ButtonEncrypt(ActionEvent actionEvent) {
+        char[] str=TextAreaInput.getText().toLowerCase().toCharArray();
+        int j=0;
+        for(int i =0;i<TextAreaInput.getText().length(); i++){
+            if(str[i]==' ') i++;
+            while(alfavitEn[j]!=str[i])
+                j++;
+            int n=j+Integer.parseInt(TextFieldKey.getText());
+            if(n<alfavitEn.length)
+                str[i]=alfavitEn[n];
+            else str[i]=alfavitEn[n-26];
+            j=0;
+        }
+        TextAreaOutput.setText(String.valueOf(str));
+    }
+
+    public void ButtonSelect(ActionEvent actionEvent) {
+    }
+
+    public void ButtonSaveAs(ActionEvent actionEvent) {
+    }
+
+    public void ButtonAbout(ActionEvent actionEvent) {
+    }
+
+    public void ButtonHowTo(ActionEvent actionEvent) {
+    }
+
+    public void ButtonLanguage(ActionEvent actionEvent) {
+    }
+
+    public void ButtonColorSheme(ActionEvent actionEvent) {
+    }
+}
+
